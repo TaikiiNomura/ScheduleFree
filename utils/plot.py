3e954279ef1c0f9a8c933df642eab9c60d967381
TaikiiNomura <23b2119@sit.shonan-it.ac.jp>
2025-06-04 21:19:00 +0900

    Update plot.py

--- utils/plot.py
+++ utils/plot.py
@@ -9,13 +9,15 @@ def plot_loss_accuracy(
         all_test_losses,
         all_test_accuracies,
         optimizer_info):
+    
     print(f'lr = {lr}')
     print(f'batch_size = {batch_size}')
+
     plt.figure(figsize=(14, 10))
 
     plt.subplot(2, 2, 1)
     for name, info in optimizer_info.items():
-        plt.plot(range(1, num_epochs + 1), all_train_losses[name][0], label=info['label'], color=info['color'])
+        plt.plot(range(1, num_epochs + 1), all_train_losses[name], label=info['label'], color=info['color'])
     plt.xlabel('Epoch')
     plt.ylabel('Train Loss')
     plt.title(f'batch_size={batch_size}')
@@ -25,7 +27,7 @@ def plot_loss_accuracy(
     # ---- 損失（テスト）----
     plt.subplot(2, 2, 2)
     for name, info in optimizer_info.items():
-        plt.plot(range(1, num_epochs + 1), all_test_losses[name][0], label=info['label'], color=info['color'])
+        plt.plot(range(1, num_epochs + 1), all_test_losses[name], label=info['label'], color=info['color'])
     plt.xlabel('Epoch')
     plt.ylabel('Test Loss')
     plt.title(f'batch_size={batch_size}')
@@ -35,7 +37,7 @@ def plot_loss_accuracy(
     # ---- 精度（訓練）----
     plt.subplot(2, 2, 3)
     for name, info in optimizer_info.items():
-        plt.plot(range(1, num_epochs + 1), all_train_accuracies[name][0], label=info['label'], color=info['color'])
+        plt.plot(range(1, num_epochs + 1), all_train_accuracies[name], label=info['label'], color=info['color'])
     plt.xlabel('Epoch')
     plt.ylabel('Train Accuracy (%)')
     plt.title(f'batch_size={batch_size}')
@@ -45,7 +47,7 @@ def plot_loss_accuracy(
     # ---- 精度（テスト）----
     plt.subplot(2, 2, 4)
     for name, info in optimizer_info.items():
-        plt.plot(range(1, num_epochs + 1), all_test_accuracies[name][0], label=info['label'], color=info['color'])
+        plt.plot(range(1, num_epochs + 1), all_test_accuracies[name], label=info['label'], color=info['color'])
     plt.xlabel('Epoch')
     plt.ylabel('Test Accuracy (%)')
     plt.title(f'batch_size={batch_size}')
