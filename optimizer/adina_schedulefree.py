48e1206f42294bc52bf419e21209f7b3567ccdcc
TaikiiNomura <23b2119@sit.shonan-it.ac.jp>
2025-06-04 19:49:44 +0900

    Update adina_schedulefree.py

--- optimizer/adina_schedulefree.py
+++ optimizer/adina_schedulefree.py
@@ -1,3 +1,17 @@
+from __future__ import print_function
+
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+from typing import Union, Tuple, Optional, Callable
+from torch.optim.optimizer import ParamsT
+
 class AdinaScheduleFree(torch.optim.Optimizer):
     def __init__(
         self,
